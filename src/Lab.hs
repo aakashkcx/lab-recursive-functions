ad7f3dc9f54d7d759c52a066fff5b586e07c5fe5
Michael Gale <m.gale@warwick.ac.uk>
2019-12-31 20:53:26 +0000

    Lab3 => Recursive functions

--- src/Lab.hs
+++ src/Lab.hs
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- Functional Programming (CS141)                                             --
--- Lab 3: Recursive and higher-order functions                                --
+-- Lab: Recursive functions                                                   --
 --------------------------------------------------------------------------------
 
 module Lab where
@@ -10,9 +10,7 @@ module Lab where
 -- Some of the functions we will be defining as part of this lab are
 -- part of Haskell's standard library. The following line tells the compiler
 -- not to import them.
-import Prelude hiding ( Monoid(..), elem, maximum, intersperse, transpose
-                      , subsequences, permutations, any, all, flip, takeWhile
-                      , zipWith, groupBy, notElem )
+import Prelude hiding ( elem, maximum, intersperse, subsequences )
 
 --------------------------------------------------------------------------------
 -- Recursive and higher-order functions
@@ -26,56 +24,7 @@ maximum = undefined
 intersperse :: a -> [a] -> [a]
 intersperse = undefined
 
-any :: (a -> Bool) -> [a] -> Bool
-any = undefined
-
-all :: (a -> Bool) -> [a] -> Bool
-all = undefined
-
-flip :: (a -> b -> c) -> b -> a -> c
-flip = undefined
-
-takeWhile :: (a -> Bool) -> [a] -> [a]
-takeWhile = undefined
-
-zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith = undefined
-
-groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
-groupBy = undefined
-
 subsequences :: [a] -> [[a]]
 subsequences = undefined
-
-permutations :: Eq a => [a] -> [[a]]
-permutations = undefined
-
---------------------------------------------------------------------------------
--- Monoids
-
--- Monoid laws:
---
--- (Left identity)      mappend mempty x = x
--- (Right identity)     mappend x mempty = x
--- (Associativity)      mappend x (mappend y z) = mappend (mappend x y) z
--- (mconcat)            mconcat = foldr mappend mempty
-
-class Monoid a where
-    mempty  :: a
-    mappend :: a -> a -> a
-    mconcat :: [a] -> a
-    mconcat = undefined
-
-instance Monoid Int where
-    mempty  = undefined
-    mappend = undefined
-
-instance Monoid [a] where
-    mempty  = undefined
-    mappend = undefined
-
-instance Monoid b => Monoid (a -> b) where
-    mempty  = undefined
-    mappend = undefined
 
 --------------------------------------------------------------------------------
